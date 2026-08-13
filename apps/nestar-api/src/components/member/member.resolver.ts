62c769f78da158e5d84993eac36bf3300c70ffb3
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-13 18:14:09 +0900

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -22,15 +22,15 @@ export class MemberResolver {
        
     }
 
-    @Mutation(() => String)
+    @Mutation(() => Member)
     @UsePipes(ValidationPipe)
-    public async login(@Args("input") input: LoginInput ): Promise<string> {
+    public async login(@Args("input") input: LoginInput ): Promise<Member> {
         try {
             console.log('Mutation: login');
-            return this.memberService.login();
+            return this.memberService.login(input);
 
         } catch(err) {
-            console.log("Error, signup:", err);
+            console.log("Error, login:", err);
             throw new InternalServerErrorException(err);
         }
     }
