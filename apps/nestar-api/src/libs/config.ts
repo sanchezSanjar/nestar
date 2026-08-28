6381435093447e29b9b27510d5586639335b2f29
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-28 17:08:07 +0900

    feat: develop single and multi image uploader mutation GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -15,6 +15,15 @@ export const availableMemberSorts = [
 	'memberViews',
 ];
 
+ // IMAGE CONFIGURATION (config.js)
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
 
 export const shapeIntoMongoObjectId = (target: any) => {
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
