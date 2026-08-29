fc2cd826fd9798daf732a8cb86310c72d92ff84f
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-29 22:34:59 +0900

    feat: develop admin related boardArticle GraphQL Api

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -4,11 +4,14 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { BoardArticleService } from './board-article.service';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
-import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import { BoardArticleInput, BoardArticlesInquiry, AllBoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
 import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import type { ObjectId } from 'mongoose';
 import { AuthMember } from '../auth/decorators/authMember.decorators';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 
 @Resolver()
@@ -57,5 +60,43 @@ export class BoardArticleResolver {
             console.log('Query getBoardArticles');
             return await this.boardArticleService.getBoardArticles(memberId, input)
         }
-}
+
+        /** ADMIN **/
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Query((returns) => BoardArticles)
+        public async getAllBoardArticlesByAdmin(
+        @Args('input') input: AllBoardArticlesInquiry,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticles> {
+        console.log('Query: getAllBoardArticlesByAdmin');
+        return await this.boardArticleService.getAllBoardArticlesByAdmin(input);
+    }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => BoardArticle)
+        public async updateBoardArticleByAdmin(
+        @Args('input') input: BoardArticleUpdate,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log('Query: updateBoardArticleByAdmin');
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.boardArticleService.updateBoardArticleByAdmin(input);
+    }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => BoardArticle)
+        public async removeBoardArticleByAdmin(
+        @Args('articleId') input: string,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<BoardArticle> {
+        console.log('Mutation: removeBoardArticleByAdmin');
+        const articleId =shapeIntoMongoObjectId(input);
+        return await this.boardArticleService.removeBoardArticleByAdmin(articleId);
+    }
+
+    }
 
--- apps/nestar-api/src/libs/dto/board-article/board-article.input.ts
+++ apps/nestar-api/src/libs/dto/board-article/board-article.input.ts
@@ -1,8 +1,9 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
-import { ObjectId } from 'mongoose';
+import type { ObjectId } from 'mongoose';
 import { BoardArticleCategory, BoardArticleStatus } from '../../enums/board-article.enum';
 import { Direction } from '../../enums/common.enum';
+import { availableBoardArticleSorts } from '../../config';
 
 @InputType()
 export class BoardArticleInput {
@@ -55,7 +56,7 @@ export class BoardArticlesInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn(['createdAt', 'updatedAt', 'articleLikes', 'articleViews'])
+	@IsIn(availableBoardArticleSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
@@ -92,7 +93,7 @@ export class AllBoardArticlesInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn(['createdAt', 'updatedAt', 'articleLikes', 'articleViews'])
+	@IsIn(availableBoardArticleSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
