81145d09830cd77fbbb3ddcb7ffe54b362f6addf
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-29 19:43:11 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -86,4 +86,15 @@ export class PropertyResolver {
         console.log('Query: getAllPropertiesByAdmin');
         return await this.propertyService.getAllPropertiesByAdmin(input);
     }
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation(() => Property)
+        public async updatePropertyByAdmin(
+        @Args('input') input: PropertyUpdate,
+    ): Promise<Property> {
+        console.log('Mutation: updatePropertyByAdmin');
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.propertyService.updatePropertyByAdmin(input);
+    }
 }
