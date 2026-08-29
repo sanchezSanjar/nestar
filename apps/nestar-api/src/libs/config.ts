9d04239e761b2edde80908f4c6bd85ec37aa0315
sanchezSanjar <sanjar@ajou.ac.kr>
2026-08-29 22:39:14 +0900

    feat: integrate comment module related DTOs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -25,7 +25,8 @@ export const availablePropertySorts = [
 	'propertyPrice',
 ]
 
-export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews']
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableCommentSorts = ['createdAt', 'updatedAt']
 
  // IMAGE CONFIGURATION (config.js)
 import { v4 as uuidv4 } from 'uuid';
