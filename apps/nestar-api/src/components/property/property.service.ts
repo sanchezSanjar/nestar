7725e72b0626b252a4d2f95c403fd03ee0be4953
sanchezSanjar <sanjar@ajou.ac.kr>
2026-09-03 20:46:48 +0900

    feat: develop likeTargetProperty GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -13,6 +13,9 @@ import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import { Direction } from '../../libs/enums/common.enum';
 import moment from 'moment';
 import { shapeIntoMongoObjectId, lookupMember  } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 
 @Injectable()
@@ -21,6 +24,7 @@ export class PropertyService {
 		@InjectModel('Property') private readonly propertyModel: Model<Property>,
 		private memberService: MemberService,
 		private viewService: ViewService,
+        private likeService: LikeService,
 	) {}
 
 	public async createProperty(input: PropertyInput): Promise<Property> {
@@ -255,6 +259,33 @@ export class PropertyService {
 
         return result[0];
         }
+
+        public async likeTargetProperty(
+		memberId: ObjectId,
+		likeRefId: ObjectId,
+	): Promise<Property> {
+		const target: Property | null = await this.propertyModel
+			.findOne({ _id: likeRefId, propertyStatus: PropertyStatus.ACTIVE })
+			.exec();
+		if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		const input: LikeInput = {
+			memberId: memberId,
+			likeRefId: likeRefId,
+			likeGroup: LikeGroup.PROPERTY,
+		};
+
+		// LIKE TOGGLE via Like modules
+		const modifier: number = await this.likeService.toggleLike(input);
+		const result = await this.propertyStatsEditor({
+			_id: likeRefId,
+			targetKey: 'propertyLikes',
+			modifier: modifier,
+		});
+
+		if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+		return result;
+	}
 
     public async updatePropertyByAdmin(
         input: PropertyUpdate,
